0210fb6422aa0b693064116a9afa9290bae37b2f
arthurire <arthurire@gmail.com>
2018-12-25 13:01:22 +0800

    Update wsj.sh

--- wsj.sh
+++ wsj.sh
@@ -2,7 +2,7 @@
 
 cd ~
 wget https://raw.githubusercontent.com/arthurire/Centos5-Firefox/master/CentOS-Base.repo --no-check-certificate
-sudo mv ~/CentOS-base.repo /etc/yum.repos.d/CentOS-Base.repo
+sudo mv ~/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo
 yum makecache && yum update
 sudo yum -y install firefox
 echo "OK!Now goto WSJ and activate your account"
